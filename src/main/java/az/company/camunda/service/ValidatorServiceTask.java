24a055e1effabcb0342899ea040cec45a8ec96a5
khayalfarzi <xeyalferziyev9@gmail.com>
2020-11-11 15:44:34 +0300

    can't solve...

--- src/main/java/az/company/camunda/service/ValidatorServiceTask.java
+++ src/main/java/az/company/camunda/service/ValidatorServiceTask.java
@@ -2,9 +2,7 @@ package az.company.camunda.service;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.springframework.stereotype.Component;
 
-@Component("validator")
 public class ValidatorServiceTask implements JavaDelegate {
 
     @Override
